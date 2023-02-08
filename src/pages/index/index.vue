472f0efa105029b944ed59daaaca6d342f6daa0e
Junjie Jin <junjie_j3@163.com>
2023-02-08 17:49:38 +0800

    feat: update page

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,22 +1,33 @@
 <template>
   <view class="index">
-    <view class="index">
     <AtNoticebar marquee>
-      欢迎使用 Taro UI Vue
+      0824版2048
     </AtNoticebar>
-    <AtButton
-      type="primary"
-      :on-click="handleClick"
-    >
-      开始新的游戏
-    </AtButton> 
+    <view class="header">
+      <AtButton
+        class="btn"
+        type="primary"
+        :on-click="init"
+      >
+        开始新的游戏
+      </AtButton>
+    </view>
+    <view v-if="isGameOver" class="game-over">
+      游戏结束
+    </view>
     <view class="content" 
-      @touchStart="handleTouchStart"
-      @touchEnd="handleTouchEnd">
-
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd">
+      <view class="item" v-for="(item,index) in numbers" :key="index">
+        <view :class="item?'number':'numberZero'"
+          :style="{backgroundImage: `url(https://cdn.jsdelivr.net/gh/chestnut824/pic/weapp/${item}.jpg)`}">
+          {{item}}
+        </view>
+      </view>
     </view>
     <AtToast :is-opened="show" :text="msg" :on-close="handleClose"></AtToast>
-  </view>
+
   </view>
 </template>
 
@@ -36,22 +47,163 @@ export default {
   data () {
     return {
       msg: 'Hello world!',
-      show: false
+      show: false,
+      startX:null,
+      startY:null,
+      endX:null,
+      endY:null,
+      direction: '',
+      threshold: 30,
+      numbers: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+      isGameOver: false,
+      tempArrs: []
     }
   },
+  created(){
+    this.init()
+  },
   methods: {
-    handleClick () {
-      this.show = true
+    init () {
+      this.isGameOver = false
+      this.numbers = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+      this.fillEmptyCell()
+      this.fillEmptyCell()
     },
     handleClose () {
       this.show = false
     },
-    handleTouchStart(e){
-      console.log(e)
+    handleTouchStart(e) {
+      this.startX = e.touches[0].pageX
+      this.startY = e.touches[0].pageY
     },
-    handleTouchEnd(e){
-      console.log(e)
+    handleTouchEnd() {
+      let deltaX = this.endX - this.startX
+      let deltaY = this.endY - this.startY
+      
+      if(Math.abs(deltaY)>Math.abs(deltaX) && deltaY>this.threshold) { //下
+        this.direction = 'down'
+      } else if(Math.abs(deltaY)>Math.abs(deltaX) && deltaY<-this.threshold) { //上
+        this.direction = 'up'
+      } else if(Math.abs(deltaY)<Math.abs(deltaX) && deltaX>this.threshold) { // 右
+        this.direction = 'right'
+      } else if(Math.abs(deltaY)<Math.abs(deltaX) && deltaX<-this.threshold) { // 左
+        this.direction = 'left'
+      }
+      this.update()
     },
+    handleTouchMove(e) {
+      this.endX = e.touches[0].pageX
+      this.endY = e.touches[0].pageY
+    },
+    fillEmptyCell() {
+      const emptyIndex = []
+      this.numbers.forEach((element, index) => {
+        if(!element) {
+          emptyIndex.push(index)
+        }
+      });
+      let randomIndex = emptyIndex[Math.floor(Math.random()*emptyIndex.length)]
+      this.numbers[randomIndex] = Math.random()>0.8?4:2
+    },
+    transformArr() {
+      let arr = [...this.numbers]
+      this.tempArrs = []
+      switch(this.direction) {
+        case 'up':
+        case 'down':
+          for(let i=0; i<4; i++) {
+            let temp = []
+            for( let j=0;j<4;j++) {
+              temp.push(arr[4*j+i])
+            }
+            this.tempArrs.push(temp)
+          }
+          break
+        case 'left':
+        case 'right':
+          for(let i=0; i<4; i++) {
+            this.tempArrs.push(arr.slice(i*4,(i+1)*4))
+          }
+          break
+        default:
+          break
+      }
+    },
+    removeZeros() {
+      for(let i=0;i<4;i++){
+        let nonZeros = this.tempArrs[i].filter(item=>item!=0)
+        let zeros = new Array(4-nonZeros.length).fill(0)
+        if(this.direction==='left'||this.direction==='up'){
+          this.tempArrs[i] = nonZeros.concat(zeros)
+        } else if(this.direction==='down'||this.direction==='right'){
+          this.tempArrs[i] = zeros.concat(nonZeros)
+        }
+      }
+      console.log(this.tempArrs)
+    },
+    addNumbers() {
+      for(let i=0;i<4;i++){
+        let arr =this.tempArrs[i]
+        if(this.direction==='left'||this.direction==='up'){
+          let j = 1
+          while(j<4) {
+            if(arr[j]===arr[j-1] && arr[j]<2048) {
+              let sum = [2*arr[j]]
+              let remainFront = arr.slice(0,j-1)
+              let remainBack = arr.slice(j+1,4)
+              this.tempArrs[i] = remainFront.concat(sum.concat(remainBack))
+              this.tempArrs[i].push(0)
+              break
+            }
+            j++
+          }
+          
+        } else if(this.direction==='down'||this.direction==='right'){
+          let j = 3
+          while(j>0){
+            if(arr[j-1]===arr[j] && arr[j]) {
+              let sum = [2*arr[j]]
+              let remainFront = arr.slice(0,j-1)
+              let remainBack = arr.slice(j+1,4)
+              this.tempArrs[i] = remainFront.concat(sum.concat(remainBack))
+              this.tempArrs[i].unshift(0)
+              break
+            }
+            j--
+          }
+          
+        }
+      }
+    },
+    recoverArr() {
+      this.numbers = []
+      switch(this.direction) {
+        case 'up':
+        case 'down':
+          for(let i=0; i<16; i++) {
+            this.numbers.push(this.tempArrs[i%4][Math.floor(i/4)])
+          }
+          break
+        case 'left':
+        case 'right':
+          for(let i=0; i<4; i++) {
+            this.numbers = this.numbers.concat(this.tempArrs[i])
+          }
+          break
+        default:
+          break
+      }
+    },
+    update() {
+      console.log(this.direction)
+      this.transformArr()
+      this.removeZeros()
+      this.addNumbers()
+      this.recoverArr()
+      this.fillEmptyCell()
+      this.fillEmptyCell()
+    },
+
   },
 }
 </script>
